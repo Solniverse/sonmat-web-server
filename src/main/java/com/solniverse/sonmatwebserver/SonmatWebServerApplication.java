c8ce8269c62fc611f4479e57d7b2a33b209992d4
depark <mem29238@gmail.com>
2023-06-09 14:03:51 +0900

    chore: Add Initial project code

---
+++ src/main/java/com/solniverse/sonmatwebserver/SonmatWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.solniverse.sonmatwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonmatWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonmatWebServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solniverse/sonmatwebserver/SonmatWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solniverse.sonmatwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonmatWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
